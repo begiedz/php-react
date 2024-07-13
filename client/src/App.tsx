29d50b84ab418b6c34bc7d19455f9b49fa2c78df
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-07-13 18:41:37 +0200

    first response

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+
+interface ApiResponse {
+  message: string
+}
+
 function App() {
+  const [response, setResonse] = useState<ApiResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost/api/')
+      .then(res => res.json())
+      .then(data => setResonse(data))
+      .catch(err => console.error(err))
+  }, [])
+
+  useEffect(() => {
+    console.log(response);
+  }, [response])
+
   return (
     <>
-      React Client
+      <h1>PHP response:</h1>
+      <p>{response?.message && response.message}</p>
     </>
   )
 }
